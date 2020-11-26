138f56d22c84466b1ff3f716f78d36318f4ba2cc
Alejandro-DP <alexdelacruz1888@gamil.com>
2020-11-26 01:22:38 -0600

    version 1

---
+++ MyAdminBD/Controllers/MyAdminController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MyAdminBD.Controllers
+{
+    public class MyAdminController : Controller
+    {
+        // GET: MyAdmin
+        public ActionResult ViewLog()
+        {
+            return View();
+        }
+    }
+}
---
+++ MyAdminBD/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="MyAdminBD.MvcApplication" Language="C#" %>
